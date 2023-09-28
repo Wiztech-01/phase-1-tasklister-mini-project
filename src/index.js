8663ae85bbabf63d1b40b4f3908817ff3c76a21a
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-28 15:01:37 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    let input = document.getElementById("new-task-description").value 
+      console.log(input);
+   let ul = document.getElementById("tasks")
+   let li = document.createElement("li")
+   li.textContent = input
+   let button = document.createElement("button")
+   button.textContent = "X"
+   button.addEventListener("click",(event)=>{
+    event.target.parentNode.remove()
+   })
+   li.appendChild(button);
+   ul.appendChild(li);
+   form.reset();
+  }); 
 });
